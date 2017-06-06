6f7cfbc2906200f386bd750d64864e6ded9dfd65
Tanechka <truefirefox@gmail.com>
2017-06-06 13:07:45 -0700

    11 05 HW10 change locale

--- src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
@@ -3,13 +3,13 @@ package ru.javawebinar.topjava.web;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import ru.javawebinar.topjava.AllActiveProfileResolver;
@@ -17,6 +17,8 @@ import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.service.UserService;
 
 import javax.annotation.PostConstruct;
+import java.util.Locale;
+
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ContextConfiguration({
@@ -26,11 +28,11 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 })
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
 @ActiveProfiles(resolver = AllActiveProfileResolver.class)
 abstract public class AbstractControllerTest {
 
     private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+    private static final Locale RU_LOCALE = new Locale("ru");
 
     static {
         CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
@@ -48,6 +50,9 @@ abstract public class AbstractControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private MessageSource messageSource;
+
     @PostConstruct
     private void postConstruct() {
         mockMvc = MockMvcBuilders
@@ -61,5 +66,9 @@ abstract public class AbstractControllerTest {
     public void setUp() {
         userService.evictCache();
         jpaUtil.clear2ndLevelHibernateCache();
+    }
+
+    protected String getMessage(String code) {
+        return messageSource.getMessage(code, null, RU_LOCALE);
     }
 }
